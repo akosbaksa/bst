9ebfb71d542f61bbbd042a4c24bc8a6c7910cebc
SimonBoldi12 <simiboldi06@gmail.com>
2025-03-23 01:23:10 +0100

    ertesites rendszer megírása

--- email.js
+++ email.js
@@ -8,7 +8,7 @@ function sendEmail() {
     const message = messageInput.value.trim();
 
     if(name === "" ||  email === "" ||  message === ""){
-        alert("Minden mező kitöltése kötelező!")
+        ertesites('hiba','Hiba - Minden mező kitöltése kötelező!')
     }else{
         if(validateEmailFormat(email)){
             const parms = {
@@ -19,18 +19,18 @@ function sendEmail() {
     
             emailjs.send("service_7j4qc3n","template_xy9mpen",parms)
             .then(() => {
-                alert("Üzenet elküldve!")
+                ertesites('sikeres','Siker - Az üzenetet sikeresen elküldted!');
                 nameInput.value = "";
                 emailInput.value = "";
                 messageInput.value = "";
             })
             .catch((error) => {
                 console.log("Hiba az üzenet elküldése közben!", error)
-                alert("Az üzenetet nem sikerült elküldeni!")
+                ertesites('hiba','Hiba - Az üzenetet nem sikerült elküldeni!')
             })
         }
         else{
-            alert("Az email címet helyes formatumban add meg! Pl.: gibszjakab@gmail.com")
+            ertesites('warning','Az email címet helyes formátumban add meg! Pl.: gibszjakab@gmail.com!')
         }
     }
 }
@@ -38,4 +38,29 @@ function sendEmail() {
 function validateEmailFormat(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
+}
+
+function ertesites(allapot,uzenet) {
+    const alertBox = document.getElementById(allapot);
+    
+    alertBox.classList.remove('hidden'); // Először megjelenítjük
+    alertBox.querySelector('p').textContent = uzenet;
+    
+    alertBox.style.height = 'auto'; // Automatikus magasság kiszámításához
+    const fullHeight = alertBox.scrollHeight + 'px'; // Előre kiszámoljuk a teljes magasságot
+    
+    alertBox.style.height = '0'; // Magasság nullára állítása, hogy az animáció látható legyen
+    
+    requestAnimationFrame(() => {
+        alertBox.style.transition = 'height 0.5s ease'; // Biztosítjuk az animációt
+        alertBox.style.height = fullHeight; // Magasság beállítása
+    });
+
+    
+    setTimeout(() => {
+        alertBox.style.height = '0'; // Visszaállítjuk a magasságot 0-ra
+        alertBox.addEventListener('transitionend', () => {
+            alertBox.classList.add('hidden'); // Eltüntetjük a dobozt, ha az animáció véget ért
+                }, { once: true }); // Csak egyszeri eseménykezelő
+            }, 4000); 
 }
